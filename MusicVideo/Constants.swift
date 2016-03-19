9fd26453b6527c6d6aa559196e600d7651de4038
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2016-03-20 02:33:48 +0800

    Part-4 completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Ayon Chowdhury on 3/20/16.
+//  Copyright © 2016 Ayon Chowdhury. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
